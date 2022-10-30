93564f4c8004bfde1154c0e98dd5422b147341fc
Max Love <maxlove@fastmail.com>
2022-10-29 20:33:28 -0400

    SAFETY_BACKUP_Sat Oct 29 08:33:28 PM EDT 2022

---
+++ polybar/polybar/launch.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# kill polybar
+killall -q polybar
+
+
+# get active monitors
+MARR="$("$HOME/bin/get_monitors.sh")"
+
+#create "basicbar" for each active monitor
+for item in ${MARR[@]}
+do
+	MONITOR=$item polybar --reload basicbar & disown
+done
+
+#---------TESTING---IGNORE-------
+# echo $MARR
+# echo ${MARR[1]}
+# polybar --reload basicbar &
+# MONITOR=polybar --reload basicbar &
+# polybar basicbar 2>&1 | tee -a /tmp/polybar.log & disown
