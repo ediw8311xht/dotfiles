91077733a06019d64f1935621887ed37e29e5297
ediw8311xht <maxlove@fastmail.com>
2023-01-13 09:40:12 -0500

    Z

---
+++ emacs/init.el
@@ -0,0 +1,36 @@
+
+;; Set up package.el to work with MELPA
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://orgmode.org/elpa/"))
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://melpa.org/packages/"))
+(setq package-enable-at-startup nil)
+(package-initialize)
+(package-refresh-contents)
+
+(unless (package-installed-p 'evil)
+  (package-install 'evil))
+(require 'evil)
+(evil-mode t)
+
+
+(add-to-list 'default-frame-alist '(alpha 100))
+(require 'w3m-load)
+(setq w3m-default-display-inline-images t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode nil)
+ '(custom-enabled-themes '(deeper-blue))
+ '(package-selected-packages '(evil-visual-mark-mode evil)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Sarasa Fixed CL" :foundry "????" :slant normal :weight bold :height 120 :width normal)))))
