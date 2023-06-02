751cad2125f720ea1d1d5371ed0910323262aac9
ediw8311xht <maxlove@fastmail.com>
2023-06-02 11:14:05 -0400

    emcas

--- emacs/init.el
+++ emacs/init.el
@@ -24,6 +24,7 @@
 ;;---------------------------------------------------------------------    SETTINGS    --------------;;
 (evil-escape-mode)
 (global-display-line-numbers-mode)
+(setq display-line-numbers-type 'relative)
 (setq scroll-step            1
       scroll-conservatively  10000)
 
@@ -101,5 +102,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Agave" :antialias true :slant normal :weight normal :height 90 :width normal)))))
-
+ '(default ((t (:family "Agave" :antialias true :slant normal :weight normal :height 105 :width normal)))))
