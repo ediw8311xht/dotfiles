19a9be4f3c25418f02a209872fed7ac22d37db54
ediw8311xht <maxlove@fastmail.com>
2023-06-02 05:19:13 -0400

    updated emacs

--- emacs/init.el
+++ emacs/init.el
@@ -1,39 +1,59 @@
-
 ;; Set up package.el to work with MELPA
 (require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "https://orgmode.org/elpa/"))
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/"))
-(add-to-list 'package-archives
-             '("melpa-stable" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "https://melpa.org/packages/"))
+
 (setq package-enable-at-startup nil)
 (package-initialize)
 (package-refresh-contents)
 
-;; Download Evil
-(unless (package-installed-p 'evil)
-  (package-install 'evil))
+(unless (package-installed-p  'evil) (package-install 'evil))
 
-;; Enable Evil
+(unless (package-installed-p 'slime) (package-install 'slime))
+
+(setq inhibit-splash-screen t)
+(setq inhibit-startup-message t)
+
 (require 'evil)
+(require 'w3m-load)
+(require 'slime)
+;; (require 'lilypond)
+
+(setq scroll-step            1
+      scroll-conservatively  10000)
+
+;;---------------------------------------------------------------------    EVIL    ------------------;;
 (evil-mode t)
-(require 'lilypond)
+
+;;--SETTINGS--;;
+(evil-set-leader 'normal (kbd "SPC"))
+
+;;--GLOBAL--;;
+;;(define-key evil-normal-state-map (kbd "<leader>]")     'next-buffer)
+
+;;--NORMAL--;;
+(define-key evil-normal-state-map (kbd "<leader>]")     'next-buffer)
+(define-key evil-normal-state-map (kbd "<leader>[") 'previous-buffer)
+(define-key evil-normal-state-map (kbd "<leader>x") 'eval-buffer)
+
+;;--INSERT--;;
+(define-key evil-insert-state-map (kbd "<tab>") "  ")
+(define-key evil-insert-state-map (kbd "C-<backspace>") (kbd "<backspace><backspace>")) 
+
+;;---------------------------------------------------------------------    EVIL    ------------------;;
 
 (add-to-list 'default-frame-alist '(alpha 100))
-(require 'w3m-load)
+(add-hook 'lisp-mode-hook          (lambda () (slime-mode t)))
+(add-hook 'inferior-lisp-mode-hook (lambda () (inferior-slime-mode t)))
+(setq evil-want-fine-undo t)
+(setq inferior-lisp-program "sbcl")
 (setq w3m-default-display-inline-images t)
+
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(blink-cursor-mode nil)
- '(custom-enabled-themes '(deeper-blue))
- '(package-selected-packages '(evil-visual-mark-mode evil)))
+ '(custom-enabled-themes '(modus-vivendi))
+ '(package-selected-packages '(slime evil-visual-mark-mode evil)))
+
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "Sarasa Fixed CL" :foundry "????" :slant normal :weight bold :height 120 :width normal)))))
+ '(default ((t (:family "Agave" :antialias true :slant normal :weight normal :height 140 :width normal)))))
