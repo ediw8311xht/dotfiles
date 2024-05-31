97b4081fa59c9f0224f6441acbdc2a69a412d331
ediw8311xht <maxlove@fastmail.com>
2024-05-31 16:08:50 -0400

    Fixes

--- doomemacs/config.el
+++ doomemacs/config.el
@@ -32,8 +32,11 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
-
+(setq doom-theme 'default)
+(setq doom-leader-key ","
+      doom-leader-alt-key ",")
+(setq  x-meta-keysym 'super
+       x-super-keysym 'meta)
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type 'relative)
@@ -43,6 +46,7 @@
 (setq org-directory "~/Documents/org_emacs/")
 
 
+(add-to-list 'default-frame-alist '(alpha 100))
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
 ;;
